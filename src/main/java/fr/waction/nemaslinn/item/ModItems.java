22bdfd2191ab2c631853040fe911e1508ce1ab4f
LesWacs <leswacs@gmail.com>
2022-10-01 17:37:11 +0200

    Titanium, Iridium, Platinium raw + resolve bug

--- src/main/java/fr/waction/nemaslinn/item/ModItems.java
+++ src/main/java/fr/waction/nemaslinn/item/ModItems.java
@@ -15,6 +15,10 @@ public class ModItems {
     public static final RegistryObject<Item> IRIDIUM_INGOT = ITEMS.register("iridium_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModTab.NEMASLINN_TAB)));
     public static final RegistryObject<Item> PLATINIUM_INGOT = ITEMS.register("platinium_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModTab.NEMASLINN_TAB)));
 
+    public static final RegistryObject<Item> RAW_TITANIUM = ITEMS.register("raw_titanium", () -> new Item(new Item.Properties().tab(ModCreativeModTab.NEMASLINN_TAB)));
+    public static final RegistryObject<Item> RAW_IRIDIUM = ITEMS.register("raw_iridium", () -> new Item(new Item.Properties().tab(ModCreativeModTab.NEMASLINN_TAB)));
+    public static final RegistryObject<Item> RAW_PLATINIUM = ITEMS.register("raw_platinium", () -> new Item(new Item.Properties().tab(ModCreativeModTab.NEMASLINN_TAB)));
+
 
     public static void register(IEventBus eventBus){
 
